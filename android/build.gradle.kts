5cd695f4142702ce6a1fe6190c92b29f752199c1
ziyafirat <ziyafirat@gmail.com>
2025-09-10 20:31:27 +0400

    Initial commit: Almaya SCO UI with common layout and header

---
+++ android/build.gradle.kts
@@ -0,0 +1,39 @@
+allprojects {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral {
+            content {
+                excludeGroupByRegex("com\\.android.*")
+                excludeGroupByRegex("com\\.google.*")
+                excludeGroupByRegex("androidx.*")
+            }
+        }
+        // Alternative repositories for better reliability
+        maven { url = uri("https://jcenter.bintray.com/") }
+        maven { url = uri("https://repo1.maven.org/maven2/") }
+    }
+}
+
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
